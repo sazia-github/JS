91f4491136844f84d79a58653b934fe51cc99c0b
Sazia Afrin <sazia-github@gmail.com>
2018-07-08 13:07:27 +0600

    first commit

---
+++ tables.js
@@ -0,0 +1,20 @@
+// var n=10
+// for (var i=1; i<=10;i++){
+//     console.log(n + '*' +i+'=' +n*i);
+    
+// }
+
+
+
+
+
+var arr=['shorna']
+arr.push('sazia')
+arr.push('afrin','shorna' )
+console.log(arr);
+
+
+arr
+
+
+var 
